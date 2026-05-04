c432c963c8db36c9f306a53ef0fa84b760b81eae
Gautam Thakur <mnrhag5@gmail.com>
2026-05-04 10:58:05 +0530

    add

--- Events/app.js
+++ Events/app.js
@@ -7,4 +7,33 @@
 // alert("button clicked")
 // }
 
-//these are too method but use eventlistnere---------------------------
+//---------------these are too method but use eventlistnere---------------------------
+
+
+
+//-----improtant Events-----
+
+//----Type.
+//----timestamp.
+//----target.
+//----toElement.
+//----currentTarget.
+//----clientX.
+//----clientY.
+//----screenX.
+//----ScreenY.
+//-----altKey.
+//----ctrlKey.
+//----shiftKey.
+//----keyCode.
+
+
+
+//--------lets remove something with event----
+
+document.querySelector(".div").addEventListener("click",function(e){
+    console.log(e)
+  let removeIt = e.target
+  removeIt.remove()
+
+})
