2af1a5d290fe8e7b54a1fded23750514e55fa91c
Gautam Thakur <mnrhag5@gmail.com>
2026-05-04 10:17:00 +0530

    add

---
+++ Events/app.js
@@ -0,0 +1,10 @@
+// document.getElementById("btn").addEventListener("click",function(){
+// alert("button clicked")
+// })
+
+
+// document.getElementById("btn").onclick =function(){
+// alert("button clicked")
+// }
+
+//these are too method but use eventlistnere---------------------------
